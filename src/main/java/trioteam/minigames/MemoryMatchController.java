ac26ba90c986b02732f2e24fbed981591be020f1
ericca2001 <44069389+ericca2001@users.noreply.github.com>
2018-10-16 13:41:51 -0400

    update
    started programming

--- src/main/java/trioteam/minigames/MemoryMatchController.java
+++ src/main/java/trioteam/minigames/MemoryMatchController.java
@@ -5,10 +5,15 @@ package trioteam.minigames;
  * Date: 10/16/18
  * Description: Memory matchning game
  */
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 
 /**
  * FXML Controller class
@@ -17,12 +22,31 @@ import javafx.fxml.Initializable;
  */
 public class MemoryMatchController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private MenuItem menuExit;
+
+    @FXML
+    private MenuItem menuInstructions;
+
+    @FXML
+    private void exit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void instructions(ActionEvent event) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Instructions");
+        alert.setHeaderText(null);
+        alert.setContentText("A series of pictures will show up on the screen, memeorize the placements of the images"
+                + "The pictures will go away after 3 seconds"
+                + "Click on the buttons to try and find pairs in the fastest time possible");
+        alert.showAndWait();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
