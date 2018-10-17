19337ea06a3ae69ba5e6b50005318e5c64a8f307
conman1005 <connercullity@live.ca>
2018-10-17 14:12:20 -0400

    Added code for selecting pokempn and opening new windows

--- src/main/java/trioteam/minigames/PokemonController.java
+++ src/main/java/trioteam/minigames/PokemonController.java
@@ -5,10 +5,18 @@ package trioteam.minigames;
  * Date: 
  * Description: 
  */
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,13 +24,30 @@ import javafx.fxml.Initializable;
  * @author Conner
  */
 public class PokemonController implements Initializable {
+    
+    @FXML
+    private void btnRandom(ActionEvent event) {
 
-    /**
-     * Initializes the controller class.
-     */
+    }
+    
+    @FXML
+    private void btnCustom(ActionEvent event) throws IOException {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/pokemonMenu.fxml")); //where FXMLPage2 is the name of the scene
+
+        Scene home_page_scene = new Scene(home_page_parent);
+        //get reference to the stage 
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.hide(); //optional
+        stage.setScene(home_page_scene); //puts the new scence in the stage
+
+        stage.setTitle("Pokemon"); //changes the title
+        stage.show(); //shows the new page
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
--- src/main/java/trioteam/minigames/PokemonMenuController.java
+++ src/main/java/trioteam/minigames/PokemonMenuController.java
@@ -6,9 +6,18 @@ package trioteam.minigames;
  * Description: 
  */
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import static trioteam.minigames.MainApp.pokemon;
 
 /**
  * FXML Controller class
@@ -16,10 +25,23 @@ import javafx.fxml.Initializable;
  * @author Conner
  */
 public class PokemonMenuController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML
+    private void btnPokemon(ActionEvent event) throws IOException {
+        Button btn = (Button) event.getSource();
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setHeaderText(btn.getText());
+        alert.setContentText("Are you sure you want to chose " + btn.getText() + "?");
+        Optional<ButtonType> result = alert.showAndWait();
+        
+        if (result.get() == ButtonType.OK) {
+            pokemon = btn.getText();
+        }
+        else {
+            
+        }
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
