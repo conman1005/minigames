fb55efb69bf18018a0cd0c3e776d5d6527b1e9fe
MandaIore <44068093+MandaIore@users.noreply.github.com>
2018-10-23 18:14:31 -0400

    collision
    Add the ground work for my Hit box collision

--- src/main/java/trioteam/minigames/SideScrollerController.java
+++ src/main/java/trioteam/minigames/SideScrollerController.java
@@ -12,9 +12,15 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.StrokeType;
 
 /**
  * FXML Controller class
@@ -25,38 +31,96 @@ public class SideScrollerController implements Initializable {
 //Health
 @FXML    
 private ImageView picHealth;
+
 //ship 
 @FXML
+private Pane panShip;
+@FXML
 private ImageView picShip;
-//Astaroid
+//Hit boxes
+@FXML
+private Rectangle recShipCol1;
+@FXML
+private Rectangle recShipCol2;
+
+//Astaroid one
+@FXML
+private Pane panAstro;
 @FXML
 private ImageView picAstaroid;
 //Hit boxes
 @FXML
-private Label lblShipCol1;
+private Rectangle recAstroCol1;
 @FXML
-private Label lblShipCol2;
+private Ellipse ovlAstroCol2;
+
+//Astaroid two
 @FXML
-private Label lblAstroCol1;
+private Pane panAstro2;
 @FXML
-private Label lblAstroCol2;
-//pane
+private ImageView picAstaroid2;
+//Hit boxes
 @FXML
-private Pane panShip;
+private Rectangle recAstroCol21;
 @FXML
-private Pane panAstro;
+private Ellipse ovlAstroCol22;
+
+//Astaroid two
+@FXML
+private Pane panAstro3;
+@FXML
+private ImageView picAstaroid3;
+//Hit boxes
+@FXML
+private Rectangle recAstroCol31;
+@FXML
+private Ellipse ovlAstroCol32;
+
+
+
+
+
         
 @FXML
 private void btnTest(ActionEvent event) throws IOException { //Test button remove before completion  
-   panAstro.setTranslateX(panAstro.getTranslateX()+5); 
+   panAstro.setTranslateX(panAstro.getTranslateX()-10); 
+  checkCol();
+  
 }
 
-private void throwAstro(){
-    
+
+private void checkCol(){
+     Shape intersect = Shape.intersect(recShipCol1, ovlAstroCol2);
+     
+       if (intersect.getBoundsInParent().getWidth() > 0) {
+           System.out.print("ObjectA intersects ObjectB"); //remove later
+        }
+       
 }
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       panShip.getChildren().add(createBoundsRectangle(recAstroCol1.getBoundsInParent()));  //make copy of astro block in ship
+        panShip.getChildren().add(createBoundsEllipse(ovlAstroCol2.getBoundsInParent()));  //make copy of astro ovel in ship
     }    
     
+    private Rectangle createBoundsRectangle(Bounds bounds) {  //method used to make the blank copy in other pane
+        Rectangle rect = new Rectangle();      
+        rect.setFill(Color.TRANSPARENT);
+        rect.setX(bounds.getMinX());
+        rect.setY(bounds.getMinY());
+        rect.setWidth(bounds.getWidth());
+        rect.setHeight(bounds.getHeight());
+        return rect;
+      }
+    private Ellipse createBoundsEllipse(Bounds bounds) {  //method used to make the blank copy in other pane
+        Ellipse ovl = new Ellipse();      
+        ovl.setFill(Color.TRANSPARENT);
+        ovl.setCenterX(bounds.getMinX());
+        ovl.setCenterY(bounds.getMinY());
+        ovl.setRadiusX(bounds.getWidth());
+        ovl.setRadiusY(bounds.getHeight());
+        return ovl;
+      }
 }
