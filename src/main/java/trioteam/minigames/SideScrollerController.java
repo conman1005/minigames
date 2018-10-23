48216db6dd3a3f9affb4b02ba48b0310294987bc
MandaIore <44068093+MandaIore@users.noreply.github.com>
2018-10-23 13:15:46 -0400

    started to test movement

--- src/main/java/trioteam/minigames/SideScrollerController.java
+++ src/main/java/trioteam/minigames/SideScrollerController.java
@@ -6,10 +6,15 @@ package trioteam.minigames;
  * Description: 
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -17,11 +22,38 @@ import javafx.scene.image.ImageView;
  * @author Spencer
  */
 public class SideScrollerController implements Initializable {
+//Health
+@FXML    
+private ImageView picHealth;
+//ship 
+@FXML
+private ImageView picShip;
+//Astaroid
+@FXML
+private ImageView picAstaroid;
+//Hit boxes
+@FXML
+private Label lblShipCol1;
+@FXML
+private Label lblShipCol2;
+@FXML
+private Label lblAstroCol1;
+@FXML
+private Label lblAstroCol2;
+//pane
+@FXML
+private Pane panShip;
+@FXML
+private Pane panAstro;
+        
+@FXML
+private void btnTest(ActionEvent event) throws IOException { //Test button remove before completion  
+   panAstro.setTranslateX(panAstro.getTranslateX()+5); 
+}
 
-ImageView picShip;
-ImageView picHealth;
-ImageView picAstaroid;
+private void throwAstro(){
     
+}
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
