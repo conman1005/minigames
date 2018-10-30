d70911c72dc5ed7a7ba1e4a6c10e289de5874bfc
conman1005 <connercullity@live.ca>
2018-10-30 13:24:15 -0400

    wackamole game added

---
+++ src/main/java/trioteam/minigames/WackAMoleController.java
@@ -0,0 +1,27 @@
+/*
+ * Made By: Conner Cullity
+ * Date: 
+ * Description: 
+ */
+package trioteam.minigames;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Conner
+ */
+public class WackAMoleController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
